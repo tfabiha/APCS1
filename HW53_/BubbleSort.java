7bbb888ec45112d54da9025437c55df2ef9338b7
tfabiha <tfabiha@stuy.edu>
2017-12-13 09:25:41 -0500

    added

---
+++ HW53_/BubbleSort.java
@@ -0,0 +1,147 @@
+/******************************
+ * class BubbleSort -- implements bubblesort algorithm (vanilla)
+ ******************************/
+
+import java.util.ArrayList;
+
+public class BubbleSort
+{
+  //=================== HELPER METHODS ===================
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi )
+  {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al )
+  {
+    int randomIndex;
+    //setup for traversal fr right to left
+    for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //======================================================
+
+  
+  // VOID version of bubbleSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void bubbleSortV( ArrayList<Comparable> data )
+  {
+    //make n-1 passes across collection
+    for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
+	    System.out.println( "commencing pass #" + passCtr + "..." );
+
+	    //iterate from first to next-to-last element
+	    for( int i = 0; i < data.size()-1; i++ ) {
+        //if element at i > element at i+1, swap
+        if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) 
+          data.set( i, data.set(i+1,data.get(i)) );	
+        //System.out.println(data); //diag: show current state of list
+	    }
+    }
+  }
+
+
+  // ArrayList-returning bubbleSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input )
+  {
+    ArrayList<Comparable> foo = new ArrayList<Comparable>();
+    for( Comparable c : input )
+      foo.add(c);
+    bubbleSortV( foo );
+    return foo;
+  }
+
+
+  public static void main( String [] args )
+  {
+    /*===============for VOID methods=============
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "ArrayList glen before sorting:\n" + glen );
+      bubbleSortV(glen);
+      System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco before sorting:\n" + coco );
+      bubbleSortV(coco);
+      System.out.println( "ArrayList coco after sorting:\n" + coco );
+      ============================================*/
+
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "ArrayList glen before sorting:\n" + glen );
+      ArrayList glenSorted = bubbleSort( glen );
+      System.out.println( "sorted version of ArrayList glen:\n" 
+      + glenSorted );
+      System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco before sorting:\n" + coco );
+      ArrayList cocoSorted = bubbleSort( coco );
+      System.out.println( "sorted version of ArrayList coco:\n" 
+      + cocoSorted );
+      System.out.println( "ArrayList coco after sorting:\n" + coco );
+    /*==========for AL-returning methods==========
+      ============================================*/
+  }//end main
+
+}//end class BubbleSort
+
+/***
+Bulbasaur by Maija Haavisto
+                                           /
+                        _,.------....___,.' ',.-.
+                     ,-'          _,.--"        |
+                   ,'         _.-'              .
+                  /   ,     ,'                   `
+                 .   /     /                     ``.
+                 |  |     .                       \.\
+       ____      |___._.  |       __               \ `.
+     .'    `---""       ``"-.--"'`  \               .  \
+    .  ,            __               `              |   .
+    `,'         ,-"'  .               \             |    L
+   ,'          '    _.'                -._          /    |
+  ,`-.    ,".   `--'                      >.      ,'     |
+ . .'\'   `-'       __    ,  ,-.         /  `.__.-      ,'
+ ||:, .           ,'  ;  /  / \ `        `.    .      .'/
+ j|:D  \          `--'  ' ,'_  . .         `.__, \   , /
+/ L:_  |                 .  "' :_;                `.'.'
+.    ""'                  """""'                    V
+ `.                                 .    `.   _,..  `
+   `,_   .    .                _,-'/    .. `,'   __  `
+    ) \`._        ___....----"'  ,'   .'  \ |   '  \  .
+   /   `. "`-.--"'         _,' ,'     `---' |    `./  |
+  .   _  `""'--.._____..--"   ,             '         |
+  | ." `. `-.                /-.           /          ,
+  | `._.'    `,_            ;  /         ,'          .
+ .'          /| `-.        . ,'         ,           ,
+ '-.__ __ _,','    '`-..___;-...__   ,.'\ ____.___.'
+ `"^--'..'   '-`-^-'"--    `-^-'`.''"""""`.,^.`.--' mh
+
+***/
+
---
+++ HW53_/MySorts.java
@@ -0,0 +1,72 @@
+public class MySorts {
+    // VOID version of bubbleSort
+    // Rearranges elements of input ArrayList
+    // postcondition: data's elements sorted in ascending order
+    public static void bubbleSortV( ArrayList<Comparable> data ) {
+	//make n-1 passes across collection
+	for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
+	    System.out.println( "commencing pass #" + passCtr + "..." );
+
+	    //iterate from first to next-to-last element
+	    for( int i = 0; i < data.size()-1; i++ ) {
+		//if element at i > element at i+1, swap
+		if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) 
+		    data.set( i, data.set(i+1,data.get(i)) );	
+		//System.out.println(data); //diag: show current state of list
+	    }
+	}
+    }
+
+    // VOID version of InsertionSort
+    // Rearranges elements of input ArrayList
+    // postcondition: data's elements sorted in ascending order
+    public static void insertionSortV( ArrayList<Comparable> data )
+    {
+	for( int partition = 1; partition < data.size(); partition++ ) {
+	    //partition marks first item in unsorted region
+
+	    //diag:
+	    System.out.println( "\npartition: " + partition + "\tdataset:");
+	    System.out.println( data ); 
+
+	    //traverse sorted region from right to left
+	    for( int i = partition; i > 0; i-- ) {
+
+		// "walk" the current item to where it belongs
+		// by swapping adjacent items
+		if ( data.get(i).compareTo( data.get(i-1) ) < 0 ) {
+		    //diag:
+		    System.out.println( "swap indices "+(i-1)+" & "+i+"..." );
+		    data.set( i, data.set( i-1, data.get(i) ) ); 
+		}
+		else 
+		    break; 
+	    }
+	}
+    }//end insertionSortV
+
+    // VOID version of SelectionSort
+    // Rearranges elements of input ArrayList
+    // postcondition: data's elements sorted in ascending order
+    public static void selectionSortV( ArrayList<Comparable> data ) 
+    {
+	//note: this version places greatest value at rightmost end,
+
+	//maxPos will point to position of SELECTION (greatest value)
+	int maxPos;
+
+	for( int pass = data.size()-1; pass > 0; pass-- ) {
+	    System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+	    maxPos = 0;
+	    for( int i = 1; i <= pass; i++ ) {
+		System.out.println( "maxPos: " + maxPos );//diag
+		System.out.println( data );//diag
+		if ( data.get(i).compareTo( data.get(maxPos) ) > 0 )
+		    maxPos = i;
+	    }
+	    data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
+	    System.out.println( "after swap: " +  data );//diag
+	}
+    }//end selectionSortV
+    
+}
